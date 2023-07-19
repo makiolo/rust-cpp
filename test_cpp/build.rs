6d2dd77588ff0346484bead909ace1be98e7689f
Ricardo Marmolejo García <makiolo@gmail.com>
2023-07-19 15:52:03 +0200

    fix build

--- test_cpp/build.rs
+++ test_cpp/build.rs
@@ -16,13 +16,19 @@ fn main() {
     // https://doc.rust-lang.org/cargo/reference/environment-variables.html
 
     let out_dir = env::var("OUT_DIR").unwrap();
-    let profile = env::var("PROFILE").unwrap();
     let package_name = env::var("CARGO_PKG_NAME").unwrap();
 
     cmake::build(".");
 
-    // println!("cargo:rustc-link-search=all={}\\build\\{}", out_dir, profile);
-    println!("cargo:rustc-link-search=all={}/build/{}", out_dir, profile);
+    if cfg!(windows) {
+        let profile = env::var("PROFILE").unwrap();
+        println!("cargo:rustc-link-search=all={}\\build\\{}", out_dir, profile);
+    } else if cfg!(unix) {
+        println!("cargo:rustc-link-search=all={}/build", out_dir);
+    } else {
+        println!("Invalid OS: {}", env::consts::OS);
+    }
+
     println!("cargo:rustc-link-lib=static={}", package_name);
 }
 
