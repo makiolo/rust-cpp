a148eebb4a711ae6a053946cd439bb3e8b4763ab
Ricardo Marmolejo <makiolo@gmail.com>
2024-03-19 17:52:56 +0100

    experiments with arrow

--- red_pandas/libs/core/python/grpc_generate.cmd
+++ red_pandas/libs/core/python/grpc_generate.cmd
@@ -1,3 +1,14 @@
 @echo off
 
-python -m grpc_tools.protoc --python_out=grpc/ --pyi_out=grpc/ --grpc_python_out=grpc/ grpc.proto
+setlocal
+
+:: https://github.com/grpc-ecosystem/awesome-grpc#tools
+
+SET mypath=%~dp0
+:: set PATH="%mypath%..\..\..\depends\protoc-25.3-win64\bin\";%PATH%
+set PATH="D:\dev\vcpkg_root\packages\protobuf_x64-windows\tools\protobuf";%PATH%
+
+python -m grpc_tools.protoc --proto_path=. --python_out=. --pyi_out=. --grpc_python_out=. grpc.proto
+protoc --proto_path=. --plugin=grpc_plugin --cpp_out=. grpc.proto
+
+endlocal
